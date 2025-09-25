ff71e56da9e7cfb2de7d99c3f05fe4a645e1a669
root <cltiwari2012@gmail.com>
2025-09-25 08:14:39 +0000

    Initial Azure VM setup with Terraform and GitHub Actions

---
+++ main.tf
@@ -0,0 +1,51 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [azurerm_network_interface.main.id]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "location" { default = "eastus" }
+variable "resource_group_name" { default = "Test_Resource" }
+variable "vnet_name" { default = "Test_Vnet" }
+variable "subnet_name" { default = "Test_subnet" }
+variable "vm_name" { default = "testvm" }
+variable "admin_username" { default = "Test" }
+variable "admin_password" { default = "Gawa850300" }
